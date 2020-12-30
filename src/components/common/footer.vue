446a31893581c8f957a88270be349cfc22139117
choyrunyu <1063978024@qq.com>
2020-12-31 00:31:00 +0800

    完成footer的开发

--- src/components/common/footer.vue
+++ src/components/common/footer.vue
@@ -1,10 +1,15 @@
 <template>
     <div id="footer">
-        footer
+        1740706140-蔡润堉 | 版权所有 | 
+        <span class="title">基于微服务的商城秒杀系统的设计与实现</span>
     </div>
 </template>
 <style lang="scss" scoped>
-
+#footer{
+    width: 100%;
+    height: 60px;
+    text-align: center;
+}
 </style>
 <script>
 export default {
